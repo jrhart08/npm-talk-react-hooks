47c0762b1df1e2d701f59099011b2e09591bacaf
Joseph Hart <josephrhart@gmail.com>
2019-12-03 20:24:34 -0600

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+npm i -D @babel/cli @babel/core @babel/preset-env @babel/preset-react eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-lodash babel-eslint
+```
---
+++ src/useKeyListener.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import includes from 'lodash/includes';
+import castArray from 'lodash/castArray';
+
+/**
+ * Usage: `useKeyListener(['Esc', 'Escape'], closeModal);`
+ */
+export default (keys, eventHandler, dependencies = []) => (
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return null;
+    }
+
+    const keyArray = castArray(keys);
+
+    const handleOnEscape = (e) => {
+      if (includes(keyArray, e.key)) {
+        eventHandler(e);
+      }
+    };
+
+    document.addEventListener('keyup', handleOnEscape);
+
+    return () => document.removeEventListener('keyup', handleOnEscape);
+  }, dependencies)
+);
